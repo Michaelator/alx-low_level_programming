04402348950c5519b970896a9d540af2eaea3ca8
Ifeanyi Nwosu <nwosuifeanyi9@gmail.com>
2022-09-08 14:53:02 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - prints the size of various types
  *
- * Retrun: always 0
+ * Retrun: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
