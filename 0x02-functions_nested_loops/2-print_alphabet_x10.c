9c456094fe512f1b3676ec9c9f3bc61c8c7682de
Ifeanyi Nwosu <nwosuifeanyi9@gmail.com>
2022-09-13 09:08:49 -0700

    Print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,6 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followedby a new line
- *
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
