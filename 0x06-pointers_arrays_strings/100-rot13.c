3c8543cf65edf1f9863ed174072d97d22bb5f2e8
Ifeanyi Nwosu <nwosuifeanyi9@gmail.com>
2022-09-22 08:56:35 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,29 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string in rot13
- * @s: string to be encoded
- *
- * Return: the resulting string
+ * rot13 - encoding a string using rot13.
+ * @s: the string to be encoded to rot13.
+ * Return: the string s encoded to rot13.
  */
+
 char *rot13(char *s)
 {
-	int i, j;
+	int i = 0, j = 0;
+	char string_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfggHhIiJjKkLlMm";
+	char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwwxyzabcdefghijklm";
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
-		for (j = 0; a[j] != '\0'; j++)
-		{
-			if (s[i] == a[j])
+		do {
+			if (s[i] == string_alpha[j])
 			{
-				s[i] = b[j];
+				s[i] = string_rot13[j];
 				break;
 			}
-		}
+			j++;
+		} while (string_alpha[j] != '\0');
+		j = 0;
+		i++;
 	}
 
 	return (s);
