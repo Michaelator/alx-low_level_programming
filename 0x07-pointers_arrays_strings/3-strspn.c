cb0b7aa0481a89ec680057bc06504c84689263a5
Ifeanyi Nwosu <nwosuifeanyi9@gmail.com>
2022-09-26 12:35:33 -0700

    substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,23 +2,28 @@
 
 /**
  * _strspn - gets the length of a prefix substring
- * @s: input string to search for substring
- * @accept: characters that prefix substring must include
- * @Return: length of prefix substring
+ * @s: the search string
+ * @accept accepttable characters
+ * @Return: length
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, a_len = 0, len = 0;
+	unsigned int count = 0;
+	char *ptr_accept = accept;
 
-	while (accept[a_len] != '\0')
-		a_len++;
-	for (i = 0; s[i] != '\0'; i++)
-		for (j = 0; j < a_len; j++)
-			if (s[i] == accept[j])
-				len++, j = a_len;
-			else
-				if (j == a_len - 1)
-					goto exit;
-exit: return (len);
+	while (*s++)
+	{
+		while (*accept++)
+			if (*(s - 1) == *(accept - 1))
+			{
+				count++;
+				break;
+			}
+		if (!(*--accept))
+			break;
+		accept = ptr_accept;
+	}
+
+	return (count);
 }
